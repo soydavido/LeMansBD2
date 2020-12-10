2d11d11b87f5a735de91337ccd3c27ba263eaf5b
soydavido <ortuod@gmail.com>
2020-12-10 17:04:50 -0400

    Nacionalidad

--- server/create.sql
+++ server/create.sql
@@ -8,10 +8,29 @@ create table public.fabricante
 
 create table public.modelo
 (
-    id numeric NOT NULL,
+    id numeric(10) NOT NULL,
     nombre varchar NOT NULL,
     id_fabricante numeric NOT NULL
     CONSTRAINT pk_modelo PRIMARY KEY (id),
     CONSTRAINT fk_modelo FOREIGN KEY (id_fabricante) REFERENCES fabricante(id)
-;)
+);
 
+create table public.equipo
+(
+    id numeric(4) NOT NULL,
+    nombre varchar NOT NULL,
+    numero_equipo numeric (3) NOT NULL,
+    activo boolean NOT NULL,
+    id_fabricante numeric NOT NULL,
+    CONSTRAINT pk_equipo PRIMARY KEY (id),
+    CONSTRAINT fk_fabricante FOREIGN KEY (id_fabricante) REFERENCES fabricante(id)
+);
+
+create table public.nacionalidad
+(
+    id numeric(3) NOT NULL,
+    gentilicio varchar(12) NOT NULL,
+    nombre_pais varchar NOT NULL,
+    foto_bandera BYTEA,
+    CONSTRAINT pk_nacionalidad PRIMARY KEY (id)
+);
