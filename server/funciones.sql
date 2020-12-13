74f26f00a86c64d4ea9658a77940e71111fcb0c1
soydavido <ortuod@gmail.com>
2020-12-12 23:29:26 -0400

    Funciones de marcas

--- server/funciones.sql
+++ server/funciones.sql
@@ -1,3 +1,5 @@
+-------------------------- CALCULOS DE MARCAS --------------------------------
+
 --Esta funcion servira para los calculos que se realizaran en el TDA MARCAS
 CREATE OR REPLACE FUNCTION minutos_a_decimales(a varchar, b float)
 RETURNS float as $$
@@ -6,3 +8,21 @@ BEGIN
 	RETURN b;
 END;
 $$LANGUAGE plpgsql
+
+--Esta funcion sirve para calcular la velocidad_promedio en base al tiempo
+CREATE OR REPLACE FUNCTION velocidad_promedio(km float, tiempo float)
+RETURNS float as $$
+BEGIN
+	RETURN km * tiempo;
+END;
+$$LANGUAGE plpgsql
+
+--Esta funcion sirve para calcular un aproximado de kilometraje por vuelta
+CREATE OR REPLACE FUNCTION kilometraje_promedio(km float, vueltas integer)
+RETURNS float as $$
+BEGIN
+	RETURN km / vueltas;
+END;
+$$LANGUAGE plpgsql
+
+
