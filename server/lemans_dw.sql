2edeffc411819a6655324f9a56b520e617373633
soydavido <ortuod@gmail.com>
2021-01-27 17:23:40 -0400

    transformaciones_equipo8

--- server/lemans_dw.sql
+++ server/lemans_dw.sql
@@ -23,8 +23,9 @@
         id_ranking numeric NOT NULL,
         vuelta_rapida varchar NOT NULL,
         velocidad_media float NOT NULL, 
-        nro_vueltas integer NOT NULL,
+        nro_vueltas numeric NOT NULL,
         kilometraje float NOT NULL,
+        posicion numeric,
         CONSTRAINT pk_dw_dim_ranking PRIMARY KEY (id_ranking)
     );
 
@@ -44,18 +45,26 @@
         CONSTRAINT pk_dw_dim_piloto PRIMARY KEY (id_piloto)
     );
 
+    CREATE TABLE dw_dim_fecha(
+        id_fecha numeric,
+        fecha date,
+        CONSTRAINT pk_dw_dim_fecha PRIMARY KEY (id_fecha)
+    );
 
     --Revisar si su PK puede ser SOLO la combinacion de las demas
     CREATE TABLE dw_hec_evento(
         tipo varchar NOT NULL,
         posicion numeric NOT NULL, 
         nro_equipo numeric NOT NULL,
-        fecha numeric NOT NULL,
+        fabricante varchar NOT NULL,
+        kilometraje varchar NOT NULL,
+        categoria varchar NOT NULL,
         id_piloto numeric NOT NULL,
         id_ranking numeric NOT NULL, 
         id_equipo numeric NOT NULL,
         id_vehiculo numeric NOT NULL,
-        CONSTRAINT pk_dw_dim_evento PRIMARY KEY (id_piloto,id_ranking,id_equipo,id_vehiculo),
+        id_fecha numeric NOT NULL,
+        CONSTRAINT pk_dw_dim_evento PRIMARY KEY (id_piloto,id_ranking,id_equipo,id_vehiculo,id_fecha),
         CONSTRAINT fk_equipo_dw_equipo FOREIGN KEY (id_equipo) REFERENCES dw_dim_equipo(id_equipo),
         CONSTRAINT fk_equipo_dw_pilotos FOREIGN KEY (id_piloto) REFERENCES dw_dim_pilotos(id_piloto),
         CONSTRAINT fk_equipo_dw_vehiculo FOREIGN KEY (id_vehiculo) REFERENCES dw_dim_vehiculo(id_vehiculo)
