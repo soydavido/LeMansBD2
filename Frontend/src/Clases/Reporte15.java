7615570a88aef4ed013d74d747ccc05b7ea91d0e
trekexol <39245161+trekexol@users.noreply.github.com>
2021-02-01 19:24:21 -0400

    frontend y parametros

---
+++ Frontend/src/Clases/Reporte15.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Reporte15 {
+    
+    
+    private String nombre;
+    
+    private String Pais;
+    
+    private int numero_victorias;
+
+    public Reporte15(String nombre, String Pais, int numero_victorias) {
+        this.nombre = nombre;
+        this.Pais = Pais;
+        this.numero_victorias = numero_victorias;
+    }
+
+    public Reporte15() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPais() {
+        return Pais;
+    }
+
+    public void setPais(String Pais) {
+        this.Pais = Pais;
+    }
+
+    public int getNumero_victorias() {
+        return numero_victorias;
+    }
+
+    public void setNumero_victorias(int numero_victorias) {
+        this.numero_victorias = numero_victorias;
+    }
+    
+    
+    
+    
+    
+    
+}
